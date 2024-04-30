9a4e2fb6cae801fc96428ce4b7379454514098ea
Andrii <shurgaiandrew335@gmail.com>
2024-04-30 13:57:05 +0300

    Перевірка на коректність прийому вхідних у program.cs із текстового файлу

--- Program.cs
+++ Program.cs
@@ -3,45 +3,18 @@ using System.Text;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ExecuteInput;
+using Block2Andrii;
 
 namespace struct_lab_student
 {
     public class Program
     {
-        static List<Student> ReadData(string fileName)
-        {
-            FileStream input;
-            List<Student> list = [];
-            try
-            {
-                input = new FileStream(fileName, FileMode.Open);
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("Неможливо відкрити файл: \n" + e.Message);
-                return list;
-            }
-            StreamReader sReader = new(input);
-            try
-            {
-                while (!sReader.EndOfStream)
-                {
-                    list.Add(new Student(sReader.ReadLine()));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Неможливо прочитати файл: \n" + e.Message);
-            }
-            return list;
-            
-        }
-
         static void Var8(List<Student> studs)
         {
             for(int i = 0; i < studs.Count; i++)
             {
-                if (    studs[i].physicsMark == '2' || studs[i].physicsMark == '-' || 
+                if (studs[i].physicsMark == '2' || studs[i].physicsMark == '-' || 
                     studs[i].mathematicsMark == '2' || studs[i].mathematicsMark == '-' || 
                     studs[i].informaticsMark == '2' || studs[i].informaticsMark == '-')
                 {
@@ -55,11 +28,12 @@ namespace struct_lab_student
         static void RunMenu(List<Student> studs)
         {
             Var8(studs);
+            Block2.Do(studs);
         }
 
         static void Main(string[] args)
         {
-            List<Student> studs = ReadData("input.txt");
+            List<Student> studs = Input.ReadData("Input.txt");
             RunMenu(studs);
         }
     }
