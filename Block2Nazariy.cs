d8188f4648e882227cee1378fb2a0ed79132e6cb
Amayori <malashenko.naz@gmail.com>
2024-04-30 20:34:29 +0300

    added block2Nazariy

---
+++ Block2Nazariy.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using struct_lab_student;
+
+namespace Block2Nazariy
+{
+    class Program
+    {
+        static void Var8(List<Student> studs)
+        {
+            for (int i = 0; i < studs.Count; i++)
+            {
+                if (studs[i].physicsMark == '2' || studs[i].physicsMark == '-' ||
+                    studs[i].mathematicsMark == '2' || studs[i].mathematicsMark == '-' ||
+                    studs[i].informaticsMark == '2' || studs[i].informaticsMark == '-')
+                {
+                    Console.WriteLine("----------------------------------------------------------------------");
+                    Console.WriteLine("Surname: " + studs[i].surName);
+                    Console.WriteLine($"Mathematics mark: {studs[i].mathematicsMark}, Physics mark: {studs[i].physicsMark}, Informatics mark: {studs[i].informaticsMark}.");
+                }
+            }
+        }
+    }
+}
