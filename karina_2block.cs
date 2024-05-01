38a0c5ab91401061398b471e82653088c5ab3bf1
Karina <karina2006.ka@gmail.com>
2024-05-01 10:49:59 +0300

    update 2 block

--- karina_2block.cs
+++ karina_2block.cs
@@ -6,39 +6,11 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace struct_lab_student
+namespace Karina2Block
 {
     partial class Program
     {
-        static List<Student> ReadData(string fileName)
-        {
-            // TODO   implement this method.
-            // It should read the file whose fileName has been passed and fill data inside array of Student
-            List <Student> list = new();
-            FileStream input;
-            try
-            {
-                input = new FileStream(fileName, FileMode.Open);
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("Неможливо відкрити файл: \n" + e.Message);
-                return list;
-            }
-            StreamReader strReader = new(input);
-            try
-            {
-                while (!strReader.EndOfStream)
-                {
-                    list.Add(new Student(strReader.ReadLine()));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Неможливо прочитати файл: \n" + e.Message);
-            }
-            return list;
-        }
+       
         static void Block2_22(List<Student>list)
         {
             DateTime localDate = DateTime.Now.Date;
@@ -57,15 +29,6 @@ namespace struct_lab_student
             }
             
         }
-        static void RunMenu(List<Student> studs)
-        {
-            Block2_22(studs);
-        }
-
-        static void Main(string[] args)
-        {
-            List<Student> studs = ReadData("input.txt");
-            RunMenu(studs);
-        }
+       
     }
 }
