41398097eec762805158ccfd2ff16578f7bbfe35
Karina <karina2006.ka@gmail.com>
2024-05-01 22:10:06 +0300

    some modify

--- obj/Debug/net8.0/laboratory4Git.AssemblyInfo.cs
+++ obj/Debug/net8.0/laboratory4Git.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laboratory4Git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55510dbbee85b3cc7233c206a571876de82fbd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c96f95d56cb16daeec38577c7b657d7c9a8fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("laboratory4Git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laboratory4Git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
